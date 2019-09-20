0f81dd35a53f21bff0a5c6613b2e57558c04f135
Nuria P <guaupelu@hotmail.com>
2019-09-20 16:24:50 +0200

    Añadido el git ignore en Seleccion Ciudades

--- SelecCiudades/introducirCiudad.js
+++ SelecCiudades/introducirCiudad.js
@@ -1,3 +1,26 @@
+var key;
+/* var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    key=myObj[0].key;
+  }
+};
+xmlhttp.open("GET", "../key.json", true);
+xmlhttp.send();
+ */
+$.ajax({
+    //Cambiar a type: POST si necesario
+    type: "GET",
+    // Formato de datos que se espera en la respuesta
+    dataType: "json",
+    // URL a la que se enviará la solicitud Ajax
+    url: "../key.json",
+})
+.done(function(response){
+    key=response[0].key;
+    console.log(response);
+});
 
 var url="";
 console.log(url);
@@ -7,7 +30,7 @@ function postalcode($cp){
     let valor = document.getElementById("ciudad").value;
     console.log("valor: ", valor);
 
-    url="https://api.openweathermap.org/data/2.5/weather?q="+valor+",es&appid=72661064c8662655acd7f910a6412c4c"
+    url="https://api.openweathermap.org/data/2.5/weather?q="+valor+",es&appid="+key+""
     recibirData();
 }
 
@@ -32,3 +55,11 @@ function recibirData(){
 
     });
 }
+
+//Pendiente de hacer:
+/*  .fail(function( jqXHR, textStatus, errorThrown ) {
+     if ( console && console.log ) {
+         console.log( "La solicitud a fallado: " +  textStatus);
+     } */
+
+// https://cybmeta.com/ajax-con-json-y-php-ejemplo-paso-a-paso
